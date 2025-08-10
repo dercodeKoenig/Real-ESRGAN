2bf1b3dd0cbcf2ec50fabd4d4afeff5433e2c15c
Marvin <marvineckhardt04092003@gmail.com>
2025-08-10 21:21:02 +0000

    .

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
 tensorboard --host 0.0.0.0 --logdir tb_logger/ &
 
-../.venv/bin/python realesrgan/train.py -opt options/train_v2_x4_m.yml --auto_resume
+#../.venv/bin/python realesrgan/train.py -opt options/train_v2_x4_m.yml --auto_resume
+#../.venv/bin/python realesrgan/train.py -opt options/train_v2_attn_x4_m.yml --auto_resume
+../.venv/bin/python realesrgan/train.py -opt options/train_v2_attn_x4_s.yml --auto_resume
