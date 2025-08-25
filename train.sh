8af2bb4466d836a02d88d8132acbe4fd8f35b096
Marvin <marvineckhardt04092003@gmail.com>
2025-08-25 13:57:09 +0000

    .

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-../.venv/bin/python realesrgan/train.py -opt options/train_v3_attn_x4_m.yml --auto_resume
+#../.venv/bin/python realesrgan/train.py -opt options/train_v3_attn_x4_m.yml --auto_resume
 ../.venv/bin/python realesrgan/train.py -opt options/ft_lowgan_moredata_v3_attn_x4_m.yml --auto_resume
 poweroff
