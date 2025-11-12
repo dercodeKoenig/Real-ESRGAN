e612fb08d89b11deee6f8b6365dfd97b8ea267ff
Marvin <marvineckhardt04092003@gmail.com>
2025-11-12 23:39:21 +0000

    .

--- realesrgan/models/realesrgan1r_model.py
+++ realesrgan/models/realesrgan1r_model.py
@@ -30,10 +30,11 @@ class RealESRGANModel1R(SRGANModel):
     def __init__(self, opt):
         super(RealESRGANModel1R, self).__init__(opt)
 
-        #self.net_g.compile(mode="max-autotune", dynamic=False, fullgraph=True)
-        #self.net_d.compile(mode="max-autotune", dynamic=False, fullgraph =True)
-        self.net_g.compile(dynamic=False, fullgraph=True)
-        self.net_d.compile(dynamic=False, fullgraph=True)  # i use this bc autotune needs me to reduce batch size from 8 to 7
+        if(opt['train'].get("use_compile", True)):
+            #self.net_g.compile(mode="max-autotune", dynamic=False, fullgraph=True)
+            #self.net_d.compile(mode="max-autotune", dynamic=False, fullgraph =True)
+            self.net_g.compile(dynamic=False)
+            self.net_d.compile(dynamic=False)  # i use this bc autotune needs me to reduce batch size from 8 to 7
 
         self.jpeger = DiffJPEG(differentiable=False).cuda()  # simulate JPEG compression artifacts
         self.usm_sharpener = USMSharp().cuda()  # do usm sharpening
