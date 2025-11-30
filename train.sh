326076e366c604372c06e21aafa8d9aca9fea40a
Marvin <marvineckhardt04092003@gmail.com>
2025-11-30 14:09:55 +0000

    .

--- train.sh
+++ train.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#../.venv/bin/python realesrgan/train.py -opt options/train_unet_dynamic_iterative_xs.yml --auto_resume
-#../.venv/bin/python realesrgan/train.py -opt options/train_unet_dynamic_iterative_res_xs.yml --auto_resume
-#../.venv/bin/python realesrgan/train.py -opt options/train_unet_dynamic_iterative_res2_xs.yml --auto_resume
-#../.venv/bin/python realesrgan/train.py -opt options/train_unet_dynamic_iterative_res3_xs.yml --auto_resume
-../.venv/bin/python realesrgan/train.py -opt options/train_unet_v2_xs.yml --auto_resume
+../.venv/bin/python realesrgan/train.py -opt options/train_unet_v3_xs.yml --auto_resume
 #poweroff
