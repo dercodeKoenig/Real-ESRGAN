e531cdfd1c8ff404a538a1e1cdaaca424596fa49
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2025-12-11 02:41:27 +0100

    Update realesrgan1r_model.py

--- realesrgan/models/realesrgan1r_model.py
+++ realesrgan/models/realesrgan1r_model.py
@@ -65,7 +65,7 @@ class RealESRGANModel1R(SRGANModel):
         print("gradient_accumulation_steps:",  self.gradient_accumulation_steps)
 
         # Track discriminator update counter and cached values
-        self.cached_d_loss_value = -1
+        self.cached_d_loss_value = -1.0
 
         # Cache tensor values instead of floats - initialize as tensors
         self.cached_d_real = torch.tensor(0.0, device=self.device)
@@ -207,7 +207,7 @@ class RealESRGANModel1R(SRGANModel):
             return False
 
         # Adaptive strategy based on cached discriminator loss from previous iteration
-        if self.cached_d_loss_value < self.d_loss_threshold and self.cached_d_loss_value != -1:
+        if self.cached_d_loss_value < self.d_loss_threshold and self.cached_d_loss_value > 0:
             # D is too strong, update less frequently
             return current_iter % self.d_slow_iters == 0
         else:
@@ -352,7 +352,7 @@ class RealESRGANModel1R(SRGANModel):
                     self.optimizer_d.step()
 
                 
-                if self.cached_d_loss_value == -1:
+                if self.cached_d_loss_value < 0:
                     self.cached_d_loss_value = d_total_loss
                 else: # smooth update to make the decision of gan weight and if it should update d more stable and not just depend on 1 batch that has noise
                     self.cached_d_loss_value = self.cached_d_loss_value * 0.99 + d_total_loss * 0.01
